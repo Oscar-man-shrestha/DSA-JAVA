e780cca0c866e9d608929bdb72f0a3d40711e711
Oscar Man Shrestha <oscar@Oscars-MacBook-Air.local>
2024-12-27 02:37:36 +0530

    Patterns

--- patterns.java
+++ patterns.java
@@ -129,3 +129,76 @@
 //     }
 // }
 
+
+//Inverted Half Pyramid With Numbers...
+
+// public class patterns{
+//     public static void main (String args[]){
+//        for (int i=1;i<=5;i++){
+//         for(int j=1;j<=5-i+1;j++){
+// System.out.print(j);
+//         }
+//         System.out.println();
+//        }
+//     }
+// }
+
+
+
+
+
+
+
+//Floyd's Triangle
+
+// public class patterns{
+//     public static void main (String args[]){
+//         int n=5;
+//         int number=1;
+//       for (int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){
+//             System.out.print(number);
+//             number++;
+//         }
+//         System.out.println();
+//       }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+//0-1 Triangle...
+
+// public class patterns{
+//     public static void main (String args[]){
+//         int n=5;
+//          for(int i=1;i<=n;i++){
+//             for (int j=1;j<=i;j++){
+//                 if((i+j)%2==0){
+//                     System.out.print("1");
+//                 }
+//                 else{
+//                     System.out.print("0");
+
+//                 }
+//             }
+// System.out.println();
+                
+//             }
+//          }
+//     }
+
+// 1
+// 01
+// 101
+// 0101
+// 10101
