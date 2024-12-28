74b40cf1cc0acf42c96dc9670a01928268fa2204
Oscar Man Shrestha <oscar@Oscars-MacBook-Air.local>
2024-12-28 18:30:35 +0530

    advance patterns

--- advanced_pattern.java
+++ advanced_pattern.java
@@ -188,25 +188,28 @@
 // 1 4 6 4 1
 
 
-public class advanced_pattern{
-    public static void main (String args []){
-        int n=5;
-        int num=1;
-        for (int i=0;i<=n;i++){
-           for (int s=1;s<=n;s++){
-            System.out.print(" ");
-           }
-           num =1;
-           for (int j=0;j<=i;j++)
-           {
-            System.out.print(num+" ");
-            num=num*(i-j)/(j+1);
-           }
+// public class advanced_pattern{
+//     public static void main (String args []){
+//         int n=5;
+//         int num=1;
+//         for (int i=0;i<=n;i++){
+//            for (int s=1;s<=n-i;s++){
+//             System.out.print(" ");
+//            }
+//            num =1;
+//            for (int j=0;j<=i;j++)
+//            {
+//             System.out.print(num+" ");
+//             num=num*(i-j)/(j+1);
+//            }
 
            
-            System.out.println();
-        }
+//             System.out.println();
+//         }
        
 
-    }
-}
+//     }
+// }
+
+
+
