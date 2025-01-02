2f75b8caac755a9f97a1e44eb9af39c705b2517a
Oscar Man Shrestha <oscar@Oscars-MacBook-Air.local>
2025-01-03 01:41:43 +0530

    array

--- array.java
+++ array.java
@@ -1,4 +1,8 @@
-// Defining an arrray..
+//                         Defining an arrray..
+
+
+
+
 
 // type[]typeName=new type[size];
 
@@ -21,25 +25,114 @@
 
 // }
 
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+//Some examples..
+
+// import java.util.*;
+// public class array{
+//     public static void main (String args []){
+
+
+//        Scanner sc=new Scanner(System.in);
+
+//        //Input size from user..
+
+//        System.out.print("Enter size of Array :");
+//        int size=sc.nextInt();
+
+//        int numbers[]=new int [size];
+
+//        //input..
+
+//         for(int i=0;i<size;i++){
+//             System.out.print("Enter Input :");
+//             numbers[i]=sc.nextInt();
+//         }
+
+//         //output..
+
+//         for(int i=0;i<size;i++){
+//             System.out.print("This is Your Output :");
+//             System.out.println(numbers[i]);
+//         }
+//     }
+// }
+
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//                                      [ Linera search ]
+
+
+// import java.util.*;
+// public class array{
+
+//     public static void main (String args []){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.print("Enter a Size :");
+//         int Size = sc.nextInt();
+
+//         int number []=new int [Size];
+
+//         //Input...
+
+//         for(int i=0;i<Size;i++){
+//             System.out.print("Enter a value for index ");
+//             number[i]=sc.nextInt();
+//         }
+
+//         //Asking which number's index you wanna know.
+
+//         System.out.print("Enter a value to find index :");
+//         int x=sc.nextInt();
+
+//         //Output...
+
+//         for(int i=0;i<Size;i++){
+
+//             if(number[i]==x){
+//                 System.out.print("index for the number is :"+i);
+//             }
+
+//         }
+
+        
+//     }
+// }
+
+
 import java.util.*;
-public class array{
-    public static void main (String args []){
-       Scanner sc=new Scanner(System.in);
-       System.out.print("Enter size of Array :");
-       int size=sc.nextInt();
-       int numbers[]=new int [size];
 
-       //input..
-        for(int i=0;i<size;i++){
-            System.out.print("Enter Input :");
-            numbers[i]=sc.nextInt();
-        }
 
-        //output..
+public class array {
+   public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+      int size = sc.nextInt();
+      int numbers[] = new int[size];
 
-        for(int i=0;i<size;i++){
-            System.out.print("This is Your Output :");
-            System.out.println(numbers[i]);
-        }
-    }
+
+      //input
+      for(int i=0; i<size; i++) {
+          numbers[i] = sc.nextInt();
+      }
+
+
+      int max = Integer.MIN_VALUE;
+      int min = Integer.MAX_VALUE;
+     
+       for(int i=0; i<numbers.length; i++) {
+           if(numbers[i] < min) {
+               min = numbers[i];
+           }
+           if(numbers[i] > max) {
+               max = numbers[i];
+           }
+       }
+
+
+       System.out.println("Largest number is : " + max);
+       System.out.println("Smallest number is : " + min);
+      
+   }
 }
---
+++ practice_array.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class practice_array {
+    public static void main (String args[]){
+        Scanner sc=new Scanner (System.in);
+        System.out.print("Enter Size :");
+        int size=sc.nextInt();
+
+        int array[]=new int [size];
+
+        //input
+        for(int i=0;i<size;i++){
+            System.out.print("Enter a array values :");
+            array[i]=sc.nextInt();
+        }
+
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+
+        //Output
+        for (int i=0;i<array.length;i++){
+            if(array[i]>max){
+                max=array[i];
+            }
+            if(array[i]<min){
+                min=array[i];
+            }
+            
+        }
+        System.out.println("The minimum value is :"+min);
+        System.out.println("The maximum value is :"+max);
+
+    }
+    
+}
+  //sabai vanda largest number tah xadaixa tara max mah save vako xa smaller ra smaller vanda ni thulo koi number halya xa vane tyo naya max xa
