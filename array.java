1114a82925f721dc1bfc292ce2b064f624249351
Oscar Man Shrestha <oscar@Oscars-MacBook-Air.local>
2025-01-04 01:18:09 +0530

    array

--- array.java
+++ array.java
@@ -102,37 +102,71 @@
 // }
 
 
-import java.util.*;
+// import java.util.*;
 
 
-public class array {
-   public static void main(String args[]) {
-      Scanner sc = new Scanner(System.in);
-      int size = sc.nextInt();
-      int numbers[] = new int[size];
+// public class array {
+//    public static void main(String args[]) {
+//       Scanner sc = new Scanner(System.in);
+//       int size = sc.nextInt();
+//       int numbers[] = new int[size];
 
 
-      //input
-      for(int i=0; i<size; i++) {
-          numbers[i] = sc.nextInt();
-      }
+//       //input
+//       for(int i=0; i<size; i++) {
+//           numbers[i] = sc.nextInt();
+//       }
 
 
-      int max = Integer.MIN_VALUE;
-      int min = Integer.MAX_VALUE;
+//       int max = Integer.MIN_VALUE;
+//       int min = Integer.MAX_VALUE;
      
-       for(int i=0; i<numbers.length; i++) {
-           if(numbers[i] < min) {
-               min = numbers[i];
-           }
-           if(numbers[i] > max) {
-               max = numbers[i];
-           }
-       }
+//        for(int i=0; i<numbers.length; i++) {
+//            if(numbers[i] < min) {
+//                min = numbers[i];
+//            }
+//            if(numbers[i] > max) {
+//                max = numbers[i];
+//            }
+//        }
 
 
-       System.out.println("Largest number is : " + max);
-       System.out.println("Smallest number is : " + min);
+//        System.out.println("Largest number is : " + max);
+//        System.out.println("Smallest number is : " + min);
       
-   }
-}
+//    }
+// }
+//sabai vanda largest number tah xadaixa tara max mah save vako xa smaller ra smaller vanda ni thulo koi number halya xa vane tyo naya max xa
+
+
+
+ import java.util.*;
+ public class array {
+    public static void main(String args[]) {
+       Scanner sc = new Scanner(System.in);
+       System.out.print(" Enter Size : " );
+       int size = sc.nextInt();
+
+
+       int numbers[] = new int[size];
+
+
+       //input
+       for(int i=0; i<size; i++) {
+           numbers[i] = sc.nextInt();
+       }
+      
+   
+        for(int i=0; i<numbers.length; i++) {
+            if(numbers[i]<numbers[i+1]){
+                System.out.println("Sorted " );
+               
+            }
+            else{
+                System.out.println("Not sorted " );
+            }
+        }
+       
+    
+    }
+ }
