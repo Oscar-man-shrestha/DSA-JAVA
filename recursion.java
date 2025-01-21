7992a05dd51f51af8383980f8dc1708721d0270a
Oscar Man Shrestha <oscar@Oscars-MacBook-Air.local>
2025-01-22 01:41:24 +0530

    recursion

--- recursion.java
+++ recursion.java
@@ -459,3 +459,33 @@
 //     }
 // }
 
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+           
+                    // Concepts imp
+
+//Recursion -Advanced Level Questions.
+
+//Print all permutations of a String.
+
+
+public class recursion {
+
+    public static void printPerm(String str,String permutation) {
+        //base case
+        if(str.length()==0){
+            System.out.println(permutation);
+            return;
+        }
+   for(int i=0;i<str.length();i++){ 
+    char currChar = str.charAt(i);
+    String newStr=str.substring(0, i)+str.substring(i+1);
+    printPerm(newStr, permutation+currChar);
+   }
+    }
+
+    public static void main(String[] args) {
+        String str="abc";
+        printPerm(str,"");
+    }
+}
+
